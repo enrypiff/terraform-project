fcb9bed0f4440a184c334b587c6ec3916d214abf
enrypiff <enricopiffer94@gmail.com>
2024-07-10 10:45:17 +0200

    first files

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+
+
+
+
+
+
+
+
